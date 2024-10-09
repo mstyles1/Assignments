d6d84ae3cb8a56a7576edc098e311f755f130b03
matt styles <styles.matt135@gmail.com>
2024-10-09 19:49:39 -0400

    Finalized assignment 1

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+
 function handleAddition() {
     let number1 = Number(document.getElementById('number1').value) 
     let number2 = Number(document.getElementById('number2').value)
-    console.log (number1 + number2)
+    document.getElementById("result").value = number1 + number2
 }
 
 function handleSubtraction() {
     let number1 = Number(document.getElementById('number1').value) 
     let number2 = Number(document.getElementById('number2').value)
-    console.log (number1 - number2)
+    document.getElementById("result").value = number1 - number2
 }
 
 function handleMultiplication() {
     let number1 = Number(document.getElementById('number1').value) 
     let number2 = Number(document.getElementById('number2').value)
-    console.log (number1 * number2)
+    document.getElementById("result").value = number1 * number2
 }
 
 function handleDivision() {
     let number1 = Number(document.getElementById('number1').value) 
     let number2 = Number(document.getElementById('number2').value)
-    console.log (number1 / number2)
+    document.getElementById("result").value = number1 / number2
     }
