d8084c6c5ef0623d6d8e9da22b8ac2c3c3b72061
matt styles <styles.matt135@gmail.com>
2024-10-09 13:05:21 -0400

    first button

---
+++ script.js
@@ -0,0 +1,13 @@
+function addition (a , b) {
+    let result = a + b;
+    result = 10
+    result = -1
+    result = true
+    return result;
+    console.log (result);
+    return result;
+}
+
+console.log (addition (1,2))
+console.log (addition (1,3))
+console.log (addition (1,4))
