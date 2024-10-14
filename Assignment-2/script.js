9e74e3f74d806197510c1d6b1b2e402086edc118
matt styles <styles.matt135@gmail.com>
2024-10-14 19:10:40 -0400

    Assignment-2 first commit

---
+++ Assignment-2/script.js
@@ -0,0 +1,10 @@
+
+
+function printDays() {
+    let myValue = document.getElementById('dayWeek');
+    if (myValue == 1) {
+        console.log ("Saturday")
+    } else if (myValue == 2) {
+        console.log ("Sunday")
+    }
+}
