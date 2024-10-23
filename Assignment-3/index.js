01ff35c24d3dbaba5d53070a0413e3bf1f4f0db4
matt styles <styles.matt135@gmail.com>
2024-10-23 13:42:19 -0400

    Images

--- Assignment-3/index.js
+++ Assignment-3/index.js
@@ -3,15 +3,31 @@ let products = [
         name: "Laptop",
         price: 999.99,
         description: "A high-performance laptop.",
-        image: "laptopt.jpeg"},
+        image: "laptop.jpeg"},
     {
         name: "Smartphone",
         price: 599.99,
         description: "A powerful smartphone.",
-        image: "smartphone.jpeg",}
+        image: "smartphone.jpeg"},
     {
         name: "Headphones",
         price: 199.99,
         description: "Noise-cancelling headphones.",
         image: "headphones.jpeg", }
-];
+]; 
+
+for (let i = 0 ; i<products.length; i++) {
+    let productItem = products[i]
+
+    let productName = document.createElement ("h2")
+    productName.textContent = productItem.name
+    let productDescription = document.createElement ("p")
+    productDescription.textContent = productItem.description
+    let productImage = document.createElement ("img")
+    productImage.src = `images/${productItem.image}`
+
+    let sectionProduct = document.getElementById("productList")
+    sectionProduct.appendChild (productName)
+    sectionProduct.appendChild (productDescription)
+    sectionProduct.appendChild (productImage)
+}
