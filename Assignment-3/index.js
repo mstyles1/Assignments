a09a726d7a63326602f77369c16eae41b613f74e
matt styles <styles.matt135@gmail.com>
2024-10-23 14:48:19 -0400

    Assignment 3

--- Assignment-3/index.js
+++ Assignment-3/index.js
@@ -3,17 +3,17 @@ let products = [
         name: "Laptop",
         price: 999.99,
         description: "A high-performance laptop.",
-        image: "laptop.jpeg"},
+        image: "laptop.jpg" },
     {
         name: "Smartphone",
         price: 599.99,
         description: "A powerful smartphone.",
-        image: "smartphone.jpeg"},
+        image: "smartphone.jpg" },
     {
         name: "Headphones",
         price: 199.99,
         description: "Noise-cancelling headphones.",
-        image: "headphones.jpeg", }
+        image: "headphones.jpg" }
 ]; 
 
 for (let i = 0 ; i<products.length; i++) {
