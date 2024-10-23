7f7f656b6a8af1cf6c8279f1e8be89581ba9602a
matt styles <styles.matt135@gmail.com>
2024-10-23 12:33:30 -0400

    As.3 First Commit

---
+++ Assignment-3/index.js
@@ -0,0 +1,17 @@
+let products = [
+    {
+        name: "Laptop",
+        price: 999.99,
+        description: "A high-performance laptop.",
+        image: "laptopt.jpeg"},
+    {
+        name: "Smartphone",
+        price: 599.99,
+        description: "A powerful smartphone.",
+        image: "smartphone.jpeg",}
+    {
+        name: "Headphones",
+        price: 199.99,
+        description: "Noise-cancelling headphones.",
+        image: "headphones.jpeg", }
+];
